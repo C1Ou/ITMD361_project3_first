866e13d26bedd964bb9558bdf296246b56d67bc6
C1Ou <252844982@qq.com>
2022-04-10 17:22:58 +0800

    add

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,14 +1,24 @@
 function init(){
   //alert('it work!');
   var el = document.getElementById('canvas');
-  var myLatLng = new google.maps.LatLng(31.236360996459716, 121.50208326905782);
+  var myLatLng = new google.maps.LatLng(31.23594677380884, 121.5010792240964);
   var mapOptions = {
     zoom:17,
     center:myLatLng,
     mapTypeId: 'satellite'
 
   };
-  var map =new google.maps.Map(el,mapOptions);
+  var myMap =new google.maps.Map(el,mapOptions);
+
+  var marker = new google.maps.Marker({
+    position:myLatLng,
+    map:myMap,
+    animation: google.maps.Animation.BOUNCE,
+    icon:'./images/shlogo.png'
+
+  });
+
+
 }
 
 
