c868dec9b1184fd3b9430a41b6116ca9c6bd598e
C1Ou <252844982@qq.com>
2022-04-10 18:37:25 +0800

    Update googlemap.js

--- js/googlemap.js
+++ js/googlemap.js
@@ -13,13 +13,36 @@ function init(){
   var marker = new google.maps.Marker({
     position:myLatLng,
     map:myMap,
-    animation: google.maps.Animation.BOUNCE,
+    draggable:true,
+    title:"Shanghai Tower",
+    animation: google.maps.Animation.DROP,
     icon:'./images/shlogo.png'
 
+  });
+  marker.addListener("click",toggleBounce);
+
+function toggleBounce(){
+  if (marker.getAnimation() !==null){
+    marker.setAnimation(null);
+  }else{
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+  var contentString = "<h1>Shanghai Tower</h1><p>Shanghai Tower is a 128-story,632-meter-tall megatall skyscraper in Lujiazui,Pudong,Shanghai.It is the world's third-tallest building by height to architectural top and it shares the record of having the world's highest observation deck within a building or structure at 562m. </p>" ;
+  var infowindow = new google.maps.InfoWindow({
+    content:contentString,
+  });
+  marker.addListener("click",() => {
+    infowindow.open({
+      map:myMap,
+      anchor:marker,
+      shouldFocus:false,
+    });
   });
 
 
 }
+
 
 
 google.maps.event.addDomListener(window, 'load', init);
