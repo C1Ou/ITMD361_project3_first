7188604d61ba519b731736ceb7e3fc7b7d440d65
C1Ou <252844982@qq.com>
2022-04-10 16:03:29 +0800

    change code

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,5 +1,14 @@
 function init(){
-  alert('it work!')；
+  //alert('it work!');
+  var el = document.getElementById('canvas');
+  var myLatLng = new google.maps.LatLng(31.236360996459716, 121.50208326905782);
+  var mapOptions = {
+    zoom:17,
+    center:myLatLng,
+    mapTypeId: 'satellite'
+
+  };
+  var map =new google.maps.Map(el,mapOptions);
 }
 
 
